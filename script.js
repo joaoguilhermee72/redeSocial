7671bc799f2b84f275d091ed0be4da3bdee78d55
joaoguilhermee72 <jnascimentodesouzasantos@gmail.com>
2026-09-09 11:47:59 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded",( )=>{
-    const likeBtn = document.querySelector(". like-btn")
+    const likeBtn = document.querySelector(". like-btn");
+const postMedia = document.querySelector(".post-media");
+if (likeBtn) return;
+
+const likesCountSpan = likeBtn.querySelector(".likes-count");
+const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+let isLiked = false;
+let baseLikes = 0;
+
+//texto inicial
+
+if (likesCountSpan){
+likesCountSpan.textContent = "0";
+}
+
+//numeros acima de 1mil
+
+function formatlikes (num){
+    if (num >=1000){
+return (num/1000).toFixed (1)+"K";
+    }
+return num.toString();
+   } 
+
+
+   
 })
